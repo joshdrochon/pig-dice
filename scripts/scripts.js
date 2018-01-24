5aaff4fab3e98018a8121af11fa44486f7a6734a
Josh Rochon and Tim Firman <josh@>
2018-01-24 10:22:44 -0800

    fixes logic, game now functions only in console

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,13 +9,6 @@ $(document).ready(function(){
   var player1score = 0;
   var player2score = 0;
   var unheldscore = 0;
-  var currentroll = dieroll();
-  if (currentroll === 1) {
-    player1turn = ! player1turn;
-    unheldscore = 0;
-  } else {
-    unheldscore += currentroll;
-  }
 
   $("button#roll").click(function(event) {
     event.preventDefault();
@@ -23,26 +16,30 @@ $(document).ready(function(){
     if (currentroll === 1) {
       player1turn = ! player1turn;
       unheldscore = 0;
+      console.log('You rolled a 1, switch players!');
     } else {
       unheldscore += currentroll;
     }
-    console.log(player1turn, unheldscore);
+    console.log(player1turn, unheldscore, player1score, player2score);
   });
 
   $("button#hold").click(function(event) {
     event.preventDefault();
     if (player1turn) {
       player1score += unheldscore;
+      unheldscore = 0;
       if (player1score >= 100) {
         alert ("Player 1 wins!");
       }
       player1turn = false;
     } else {
       player2score += unheldscore;
+      unheldscore = 0;
       if (player2score >= 100) {
         alert ("Player 2 wins!");
       }
       player1turn = true;
     };
+    console.log(player1turn, unheldscore, player1score, player2score);
   });
 });
